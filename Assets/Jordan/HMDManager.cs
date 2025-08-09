6a49cfdc2a19b4294dedf585e8c01a66ba0f64e5
Jordan Muniz <jordy.valois@gmail.com>
2025-08-09 14:29:20 -0400

    Add Delay to Don and Doff
    - Added the delay and did some cleanup

--- Assets/Jordan/HMDManager.cs
+++ Assets/Jordan/HMDManager.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
 public class HMDManager : MonoBehaviour
 {
-    public GameObject HMDhitbox;
+    public GameObject HMDHitbox;
+    public XRSocketInteractor socketInteractor;
+    public Stack<GameObject> HMDStack = new Stack<GameObject>();
 
     void Start()
     {
@@ -12,27 +16,49 @@ public class HMDManager : MonoBehaviour
         Camera mainCamera = Camera.main;
         if (mainCamera != null)
         {
-            HMDhitbox.transform.parent = mainCamera.transform;
-            HMDhitbox.transform.localPosition = Vector3.zero;
+            HMDHitbox.transform.parent = mainCamera.transform;
+            HMDHitbox.transform.localPosition = Vector3.zero;
         }
         else
         {
             Debug.LogWarning("Main Camera not found in the scene.");
         }
+
+        if (socketInteractor != null)
+        {
+            socketInteractor.selectEntered.AddListener(DonWaitAndProcess);
+            socketInteractor.selectExited.AddListener(ProcessHeadsetDoff);
+        }
+    }
+    private void DonWaitAndProcess(SelectEnterEventArgs args)
+    {
+        StartCoroutine(DelayedHeadsetDon(args));
     }
 
-    void Update()
+    private IEnumerator DelayedHeadsetDon(SelectEnterEventArgs args)
     {
-
+        yield return new WaitForSeconds(2f);
+        ProcessHeadsetDon(args);
     }
 
-    public void HandleHeadsetDon()
+    private void ProcessHeadsetDon(SelectEnterEventArgs args)
     {
+        // Fire the headset don event
         EventManager.HeadsetDon();
+
+        // Get the GameObject that was slotted
+        GameObject slottedObject = args.interactableObject.transform.gameObject;
     }
 
-    public void HandleHeadsetDoff()
+    private void ProcessHeadsetDoff(SelectExitEventArgs args)
     {
+        // Fire the headset doff event
         EventManager.HeadsetDoff();
+
+        // Get the GameObject that was removed
+        GameObject removedObject = args.interactableObject.transform.gameObject;
+
+        Debug.Log($"Object removed: {removedObject.name}");
+
     }
 }
