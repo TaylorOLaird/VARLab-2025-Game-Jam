1a40666b296138acb4f8015abb7c7fba891a8edd
JudahRowe <122582641+JudahRowe@users.noreply.github.com>
2025-08-09 12:42:36 -0400

    Added Waterworks asset and water logic

---
+++ Assets/Judah/Scripts/WaterManager.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class WaterManager : MonoBehaviour
+{
+
+    [SerializeField] private GameObject waterPlane;   
+
+    public void EnableWater()
+    {
+        waterPlane.SetActive(true);
+        //RenderSettings.fog = true;
+        //RenderSettings.fogMode = FogMode.ExponentialSquared;
+        //RenderSettings.fogColor = new Color(0.0f, 0.4f, 0.7f); 
+        //RenderSettings.fogDensity = 0.05f; 
+        WaterSceneState.isWaterEnabled = true;
+    }
+
+    public void DisableWater()
+    {
+        waterPlane.SetActive(false);
+      //  RenderSettings.fog = false;
+        WaterSceneState.isWaterEnabled = false;
+    }
+
+    private void OnEnable()
+    {
+        EnableWater();
+    }
+
+    private void OnDisable()
+    {
+        DisableWater();
+    }
+}
---
+++ Assets/Judah/Scripts/WaterSceneState.cs
@@ -0,0 +1,3 @@
+public static class WaterSceneState {
+    public static bool isWaterEnabled = false;
+}
