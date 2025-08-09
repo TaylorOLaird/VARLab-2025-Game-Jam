9e5a9ef8e23a44321d668924c67f70fbb61e009e
Taylor Laird <ta187904@ucf.edu>
2025-08-09 18:58:17 -0400

    set up follower ui with hmd dof

--- Assets/Taylor/Scripts/Menu.cs
+++ Assets/Taylor/Scripts/Menu.cs
@@ -6,8 +6,8 @@ using UnityEngine.InputSystem;
 public class Menu : MonoBehaviour
 {
     [SerializeField] private InputActionReference openMenuButton;
-    [SerializeField] private InputActionReference testDONButton;
-    [SerializeField] private InputActionReference testDOFFButton;
+    // [SerializeField] private InputActionReference testDONButton;
+    // [SerializeField] private InputActionReference testDOFFButton;
     private bool isMenuOpen = false;
 
     [SerializeField] private GameObject menuUI;
@@ -15,6 +15,8 @@ public class Menu : MonoBehaviour
     
     [SerializeField] private List<GameObject> headsetImages;
     private int headsetIndex = 0;
+
+    [SerializeField] private HMD testHMD;
 
     void Start()
     {
@@ -35,13 +37,12 @@ public class Menu : MonoBehaviour
 
         openMenuButton.action.Enable();
         openMenuButton.action.performed += toggleMenu;
-        testDONButton.action.Enable();
-        testDONButton.action.performed += addHeadset;
-        testDOFFButton.action.Enable();
-        testDOFFButton.action.performed += removeHeadset;
-
-        // EventManager.OnHeadsetDon += addHeadset;
-        // EventManager.OnHeadsetDoff += removeHeadset;
+        // testDONButton.action.Enable();
+        // testDONButton.action.performed += tmpAddHeadset;
+        // testDOFFButton.action.Enable();
+        // testDOFFButton.action.performed += tmpRemoveHeadset;
+        EventManager.OnHeadsetDon += addHeadset;
+        EventManager.OnHeadsetDoff += removeHeadset;
     }
 
     void Update()
@@ -49,13 +50,29 @@ public class Menu : MonoBehaviour
 
     }
 
+    // public void tmpAddHeadset(InputAction.CallbackContext context)
+    // {
+    //     addHeadset(testHMD);
+    // }
+    // public void tmpRemoveHeadset(InputAction.CallbackContext context)
+    // {
+    //     removeHeadset(testHMD);
+    // }
+
     public void addHeadset(HMD headset)
     {
         if (headsetIndex < headsetImages.Count)
         {
             headsetImages[headsetIndex].SetActive(true);
+            HMD hmdScript = headset.GetComponent<HMD>();
+            headsetImages[headsetIndex].GetComponent<SpriteRenderer>().sprite = hmdScript.headsetSprite;
             headsetIndex++;
         }
+        else
+        {
+            Debug.Log("Maximum number of headsets reached.");
+        }
+        
     }
 
     public void removeHeadset(HMD headset)
@@ -64,6 +81,10 @@ public class Menu : MonoBehaviour
         {
             headsetIndex--;
             headsetImages[headsetIndex].SetActive(false);
+        }
+        else
+        {
+            Debug.Log("No headsets to remove.");
         }
     }
 
