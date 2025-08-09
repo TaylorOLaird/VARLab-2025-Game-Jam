775b33fb074dbb5d8c8b24e7f6bf0b1c8949d964
JudahRowe <122582641+JudahRowe@users.noreply.github.com>
2025-08-09 15:59:47 -0400

    Added Dissolving/Rematerializing Logic

--- Assets/Judah/Scripts/WaterSceneState.cs
+++ Assets/Judah/Scripts/WaterSceneState.cs
@@ -1,3 +1,19 @@
-public static class WaterSceneState {
-    public static bool isWaterEnabled = false;
+public static class WaterSceneState
+{
+    private static bool _isWaterEnabled;
+
+    public static event System.Action<bool> OnWaterEnabledChanged;
+
+    public static bool IsWaterEnabled
+    {
+        get => _isWaterEnabled;
+        set
+        {
+            if (_isWaterEnabled != value) // only trigger if actually changed
+            {
+                _isWaterEnabled = value;
+                OnWaterEnabledChanged?.Invoke(_isWaterEnabled);
+            }
+        }
+    }
 }
