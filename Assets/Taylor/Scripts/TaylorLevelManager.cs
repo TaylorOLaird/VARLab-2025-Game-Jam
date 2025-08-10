125131f7066b42272b96de3c12f55eb54e5e2d32
Taylor Laird <ta187904@ucf.edu>
2025-08-09 20:34:19 -0400

    scene loading setting

--- Assets/Taylor/Scripts/TaylorLevelManager.cs
+++ Assets/Taylor/Scripts/TaylorLevelManager.cs
@@ -2,33 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
 
 public class TaylorLevelManager : MonoBehaviour
 {
-    [SerializeField] private InputActionReference simulateDonHMDButton;
-    [SerializeField] private InputActionReference simulateDofHMDButton;
+    [SerializeField] private Volume volume;
+
     // Start is called before the first frame update
     void Start()
     {
-        simulateDonHMDButton.action.Enable();
-        simulateDofHMDButton.action.Enable();
-        simulateDonHMDButton.action.performed += SimulateDonHMD;
-        simulateDofHMDButton.action.performed += SimulateDofHMD;
-    }
+        EventManager.OnHeadsetDon += addHeadset;
+        EventManager.OnHeadsetDoff += removeHeadset;
+        volume.enabled = false;
 
-    private void SimulateDonHMD(InputAction.CallbackContext context)
-    {
-        Debug.Log("Simulating HMD Don");
-    }
-
-    private void SimulateDofHMD(InputAction.CallbackContext context)
-    {
-        Debug.Log("Simulating HMD Doff");
+        SceneManager.LoadSceneAsync("Assets/Taylor/Scenes/Lab.unity", LoadSceneMode.Additive);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void addHeadset(HMD headset)
+    {
+        Debug.Log("Headset added - B&W enabled");
+        volume.enabled = true;
+        SceneManager.LoadSceneAsync("Assets/Taylor/Scenes/Old.unity", LoadSceneMode.Additive);
+        SceneManager.UnloadSceneAsync("Assets/Taylor/Scenes/Lab.unity");
+    }
+
+    public void removeHeadset(HMD headset)
+    {
+        Debug.Log("Headset removed - B&W disabled");
+        volume.enabled = false;
+        SceneManager.LoadSceneAsync("Assets/Taylor/Scenes/Lab.unity", LoadSceneMode.Additive);
+        SceneManager.UnloadSceneAsync("Assets/Taylor/Scenes/Old.unity");
     }
 }
