5325604f155dcec7d5498a465ea8af9aedf36eec
Guufla <146783384+Guufla@users.noreply.github.com>
2025-08-10 19:22:28 -0400

    Sprites

--- Assets/Liam/Scripts/WallAlign.cs
+++ Assets/Liam/Scripts/WallAlign.cs
@@ -17,6 +17,7 @@ public class WallAlign : MonoBehaviour
         {
             if (other.CompareTag("Floor"))
             {
+                
             transform.position = new Vector3(other.transform.position.x, transform.position.y - 0.02f, other.transform.position.z);
             isAligned = true;
             }
