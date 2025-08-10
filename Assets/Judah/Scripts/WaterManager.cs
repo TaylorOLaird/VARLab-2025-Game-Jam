fe1d0ac9762ad40c87a7c02eed401a62a5eb214e
JudahRowe <122582641+JudahRowe@users.noreply.github.com>
2025-08-10 12:33:23 -0400

    Improved current logic

--- Assets/Judah/Scripts/WaterManager.cs
+++ Assets/Judah/Scripts/WaterManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 
@@ -8,12 +9,26 @@ public class WaterManager : MonoBehaviour
     [SerializeField] private Volume volume;
     [SerializeField] private Color color;
     [SerializeField] private float fogDensity;
+    [SerializeField] private GameObject currents;
+    private List<Transform> currentPivots;
 
+    private void Start()
+    {
+        EventManager.OnHeadsetDon += HandleHeadsetDon;
+        EventManager.OnHeadsetDoff += HandleHeadsetDoff;
+
+        currentPivots = new List<Transform>();
+        foreach (Transform child in currents.transform)
+        {
+            currentPivots.Add(child);
+        }
+    }
     private void Update()
     {
         RenderSettings.fogColor = color;
         RenderSettings.fogDensity = fogDensity;
     }
+    [ContextMenu("EnableWater")]
     public void EnableWater()
     {
         moveNormal.SetActive(false);
@@ -25,7 +40,7 @@ public class WaterManager : MonoBehaviour
         RenderSettings.fogDensity = fogDensity;
         WaterSceneState.IsWaterEnabled = true;
     }
-
+    [ContextMenu("DisableWater")]
     public void DisableWater()
     {
         moveNormal.SetActive(true);
@@ -35,13 +50,46 @@ public class WaterManager : MonoBehaviour
         WaterSceneState.IsWaterEnabled = false;
     }
 
-    private void OnEnable()
+    [ContextMenu("ReverseCurrents")]
+    public void ReverseCurrents()
     {
-        EnableWater();
+        foreach (Transform pivot in currentPivots)
+        {
+            pivot.localEulerAngles = new Vector3(0f, 180f, 0f);
+        }
     }
 
-    private void OnDisable()
+    [ContextMenu("RestoreCurrents")]
+    public void RestoreCurrents()
     {
-        DisableWater();
+        foreach (Transform pivot in currentPivots)
+        {
+            pivot.localEulerAngles = new Vector3(0f, 0f, 0f);
+        }
+    }
+
+    public void HandleHeadsetDon(HMD headset)
+    {
+        if (headset.gameObject.name == "HMDWater")
+        {
+            EnableWater();
+        }
+        else if (headset.gameObject.name == "HMDCurrentFlow")
+        {
+            ReverseCurrents();
+        }
+        
+    }
+
+    public void HandleHeadsetDoff(HMD headset)
+    {
+        if (headset.gameObject.name == "HMDWater")
+        {
+            DisableWater();
+        }
+        else if (headset.gameObject.name == "HMDCurrentFlow")
+        {
+            RestoreCurrents();
+        }
     }
 }
